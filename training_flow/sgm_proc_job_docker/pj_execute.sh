c051400f10f03362570b9673a77ea2276ed3389f
Leandro Vidal <lvidal@humansystems.com>
2024-09-16 12:21:34 -0300

    adjust permissions for script

--- training_flow/sgm_proc_job_docker/pj_execute.sh
+++ training_flow/sgm_proc_job_docker/pj_execute.sh
@@ -4,7 +4,10 @@ WORKDIR=/opt/ml/processing
 
 cd ${WORKDIR}/ml-training-flow
 
+echo "Transforming notebook into script..."
 jupyter nbconvert --to script notebook.ipynb
+echo "Executing notebook..."
 python notebook.py
 
+echo "Saving results to local S3 Bucket dir..."
 cp -r CNN-Intent-Classifier-0.0.2.mlpackage ${WORKDIR}/output-data/
